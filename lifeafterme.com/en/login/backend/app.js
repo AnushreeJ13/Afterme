866df1ba3289cebdd3a21571d693ff49aadf0dcc
AnushreeJ13 <Jainanushree2005@gmail.com>
2026-01-31 12:01:57 +0530

    added login link in backend

--- lifeafterme.com/en/login/backend/app.js
+++ lifeafterme.com/en/login/backend/app.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "http://127.0.0.1:5501"
+    "http://127.0.0.1:5501",
+    "https://afterme-login.vercel.app"
   ],
   credentials: true
 }));
